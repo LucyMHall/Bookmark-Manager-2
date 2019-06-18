a0cdf60d27d1b072a1fffcdad983e0e9a55c8fdd
LucyMHall <lucymhall1995@gmail.com>
2019-06-18 17:32:38 +0100

    connect to the bookmark database when calling .all on Bookmark

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,8 +3,10 @@ require 'bookmark'
 describe Bookmark do
   describe '::all' do
     it 'should return all instances of bookmark' do
-      bookmarks = ['www.google.com']
-      expect(Bookmark.all).to eq(bookmarks)
+      @bookmarks = ["http://www.makersacademy.com", "http://www.google.com", "http://www.destroyallsoftware.com"]
+      expect(Bookmark.all).to include("http://www.makersacademy.com")
+      expect(Bookmark.all).to include("http://www.google.com")
+      expect(Bookmark.all).to include("http://www.destroyallsoftware.com")
     end
   end
 end
--- spec/features/view_bookmarks_page_spec.rb
+++ spec/features/view_bookmarks_page_spec.rb
@@ -1,8 +1,8 @@
 feature 'view bookmarks' do
   scenario 'view a list of bookmarked URLs' do
     visit('/bookmarks')
-    expect(bookmarks).to include("http://www.makersacademy.com")
-    expect(bookmarks).to include("http://www.google.com")
-    expect(bookmarks).to include("http://www.destroyallsoftware.com")
+    expect(page).to have_content("http://www.makersacademy.com")
+    expect(page).to have_content("http://www.google.com")
+    expect(page).to have_content("http://www.destroyallsoftware.com")
   end
 end
