1bb090ab982fbf362d59c323d9791d3a65dd3a27
LucyMHall <lucymhall1995@gmail.com>
2019-06-19 20:35:19 +0100

    the test database will be accessed when testing not the real database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,8 +2,13 @@ require 'pg'
 
 class Bookmark
   def self.all
-    connection = PG.connect(dbname: 'bookmark_manager')
-    result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark["url"] }
+    if ENV['ENVIRONMENT'] = 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+      result = connection.exec("SELECT * FROM bookmarks;")
+      @array_of_bookmarks = result.map { |bookmark| bookmark["url"] }
+      @array_of_bookmarks
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,9 +1,15 @@
 require 'bookmark'
+require 'spec_database_helper.rb'
 
 describe Bookmark do
+
   describe '::all' do
     it 'should return all instances of bookmark' do
-      @bookmarks = ["http://www.makersacademy.com", "http://www.google.com", "http://www.destroyallsoftware.com"]
+
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
       expect(Bookmark.all).to include("http://www.makersacademy.com")
       expect(Bookmark.all).to include("http://www.google.com")
       expect(Bookmark.all).to include("http://www.destroyallsoftware.com")
